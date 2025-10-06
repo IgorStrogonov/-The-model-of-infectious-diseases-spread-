02f6bb8d72779cf5dc30f55d50ef518e746fced9
IgorStrogonov <strogij27@gmail.com>
2025-10-07 00:29:13 +0300

    simulation logging added

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ import simpy
 from metrics.sir import SIRMetricsCollector
 from models.sir import SIRBasicAgent
 
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
 MSK = zoneinfo.ZoneInfo("Europe/Moscow")
 
 
@@ -25,6 +35,11 @@ SIM_TIME = 365
 random.seed(RANDOM_SEED)
 
 if __name__ == '__main__':
+    logger.info(
+        f"Simulation has started with params: --random_seed {RANDOM_SEED} --beta {BETA} --gamma {GAMMA} --n_agents {N_AGENTS} --sim_time {SIM_TIME}"
+    )
+    start = time.time()
+
     env = simpy.Environment()
     agents: Dict[int, SIRBasicAgent] = {
         n: SIRBasicAgent(env=env, name=f"A_{n}", beta=BETA, gamma=GAMMA)
@@ -38,6 +53,9 @@ if __name__ == '__main__':
     env.process(collector.run())
 
     env.run(until=SIM_TIME)
+
+    finish = time.time()
+    logger.info(f"Simulation has finished. Elapsed time {(finish - start):.3f} seconds.")
 
     simulation_ts = msk_now_str()
     collector.to_csv(f"simulation_data/SIR_SimulationData-{simulation_ts}.csv")
